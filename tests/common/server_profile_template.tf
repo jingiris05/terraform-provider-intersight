88ebf10ad9f7c216459e3204807861daeca2c4b2
jingiris05 <167278817+jingiris05@users.noreply.github.com>
2026-07-17 15:51:04 +0530

    Update server_profile_template.tf

--- tests/common/server_profile_template.tf
+++ tests/common/server_profile_template.tf
@@ -2,7 +2,8 @@
 resource "intersight_server_profile_template" "template1" {
   name            = "server_profile_template1"
   description     = "demo server profile template"
-  target_platform = "FIAttached"
+  target_platform = "Standalone"
+  server_family   = "UCSC2XX/4XX"
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.default.results.0.moid
