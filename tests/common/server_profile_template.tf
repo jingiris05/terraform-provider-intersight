8240bd74aa807ab0599fe3920160d260df24b9af
JINDAT BARADIA GIRISH -X (jingiris - XORIANT SOLUTIONS PRIVATE LIMITED at Cisco) <jingiris@cisco.com>
2026-07-31 13:26:07 +0530

    Updating test

--- tests/common/server_profile_template.tf
+++ tests/common/server_profile_template.tf
@@ -2,8 +2,7 @@
 resource "intersight_server_profile_template" "template1" {
   name            = "server_profile_template1"
   description     = "demo server profile template"
-  target_platform = "Standalone"
-  server_family   = "UCSC2XX/4XX"
+  target_platform = "FIAttached"
   organization {
     object_type = "organization.Organization"
     moid        = data.intersight_organization_organization.default.results.0.moid
