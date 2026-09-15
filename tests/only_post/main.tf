3ce1654ebdcaad3e0d24e9704e71f1d9ed92baaf
jingiris05 <167278817+jingiris05@users.noreply.github.com>
2026-09-15 22:46:23 +0530

    Update main.tf

--- tests/only_post/main.tf
+++ tests/only_post/main.tf
@@ -113,7 +113,9 @@ resource "intersight_server_profile" "server_profile1"{
 	name = "demotesting_DERIVED-5"
 	description = "Sample description"
 	target_platform = "FIAttached"
-	server_family   = "All"
+	additional_properties = jsonencode({
+        ServerFamily = "All"
+    })
   organization {
      object_type = "organization.Organization"
      moid        = data.intersight_organization_organization.default.results.0.moid
